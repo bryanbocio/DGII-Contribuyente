d47a37439cfc45c3364e4167f4537fcfd2ddef28
bryanbocio <alexanderbocio@gmail.com>
2024-03-15 23:32:02 -0400

    agregando dbsets para poder realizar migracion

--- Infrastructure/Data/DbContextDGII.cs
+++ Infrastructure/Data/DbContextDGII.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities.Contribuyentes;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Core.Data
@@ -11,6 +12,9 @@ namespace Core.Data
 
         }
 
+        public DbSet<TipoContribuyente> TiposContribuyente { get; set; }
+        public DbSet<Contribuyente> Contribuyentes { get; set; }
+        public DbSet<ComprobanteFiscal> ComprobantesFiscales{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
