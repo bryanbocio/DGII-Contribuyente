5316699fc74fa3dbdb105fcc8e2ea72179fa4e0a
bryanbocio <alexanderbocio@gmail.com>
2024-03-15 23:29:38 -0400

    agregando Startup personalizado

---
+++ API/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Extensions;
+using Core.Data;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuracion;
+        public Startup(IConfiguration configuration)
+        {
+            _configuracion = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<DbContextDGII>(options => options.UseSqlite(_configuracion.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerDocumentation();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseSwaggerDocumentation();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
