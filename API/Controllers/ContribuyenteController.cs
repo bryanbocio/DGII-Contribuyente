084acb1779d41bee0ca192d6bde007d87bd932bf
bryanbocio <alexanderbocio@gmail.com>
2024-03-16 10:57:41 -0400

    Agregando controllador, para probar si se ingresó correctamente la data

---
+++ API/Controllers/ContribuyenteController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Data;
+using Core.Entities.Contribuyentes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ContribuyenteController : Controller
+    {
+
+        private readonly DbContextDGII dbContext;
+        public ContribuyenteController(DbContextDGII context)
+        {
+            this.dbContext = context;
+        }
+
+
+        [HttpGet("/contribuyentes")]
+        public async Task<List<Contribuyente>> GetContribuyentes()
+        {
+           return await dbContext.Contribuyentes.Include(contri=> contri.TipoContribuyente).ToListAsync();
+        }
+    }
+}
