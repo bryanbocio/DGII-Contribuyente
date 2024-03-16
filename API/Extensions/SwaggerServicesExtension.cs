324a0b1fec3f2cf8ac74f6b1300d6853fb99973d
bryanbocio <alexanderbocio@gmail.com>
2024-03-15 23:30:07 -0400

    agregando un Extension de Swagger para poder separar los servicios y tener mejor organizacion

---
+++ API/Extensions/SwaggerServicesExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Extensions
+{
+    public static class SwaggerServicesExtension
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+
+            return app;
+        }
+
+    }
+}
