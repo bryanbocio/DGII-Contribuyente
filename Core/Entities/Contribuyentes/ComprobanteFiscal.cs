8ea3c5d2b3a3297d97930da1b1931b16fbd55e75
bryanbocio <alexanderbocio@gmail.com>
2024-03-15 20:35:02 -0400

    agregando entidades base

---
+++ Core/Entities/Contribuyentes/ComprobanteFiscal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Contribuyentes
+{
+    public class ComprobanteFiscal : BaseEntity
+    {
+        public int ContribuyenteId  { get; set; }
+        public Contribuyente Contribuyente  { get; set; }   
+        public string Ncf { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Monto { get; set; }
+
+        public decimal GetItbis()
+        {
+            return this.Monto * (decimal)0.18;
+        }
+        
+    }
+}
---
+++ Core/Entities/Contribuyentes/Contribuyente.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Core.Entities.Contribuyentes
+{
+    public class Contribuyente : BaseEntity
+    {
+        public string RncCedula { get; set; }
+        public string Nombre { get; set; }
+        public IReadOnlyList<ComprobanteFiscal> ComprobantesFiscales { get; set; }
+        public int  TipoContribuyenteId { get; set; }
+        public TipoContribuyente TipoContribuyente { get; set; }
+        public bool IsActivo { get; set; }
+      
+
+
+
+    }
+}
