6c76d17500502d623650a19b2416879cee0ae2db
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 15:33:30 -0400

    Agregando controladores de contribuyente y de comprobante

---
+++ API/Controllers/ComprobanteFiscal/ComprobantesFiscalController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Core.Entities.Contribuyentes;
+using Core.Data;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using API.DTOs;
+using Core.Specification.Comprobantes;
+using API.Utils;
+using API.Helpers;
+
+namespace API.Controllers.ComprobanteController
+{
+    public class ComprobanteController : BaseApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ComprobanteController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("/comprobante-fiscal")]
+        public async Task<ActionResult<Pagination<ComprobanteFiscalToReturnDto>>> buscar([FromQuery] ComprobanteSpecificationParameters parameters)
+        {
+            var comprobantes = await _unitOfWork.Repository<ComprobanteFiscal>().ListAsync(new ComprobanteWithContribuyenteSpecification(parameters));
+            var lstComprobantesFiscalDto = new List<ComprobanteFiscalToReturnDto>();
+            var count= await _unitOfWork.Repository<ComprobanteFiscal>().CountAsync(new ComprobanteWithContribuyenteSpecification(parameters));
+            foreach(var item in comprobantes)
+            {
+                lstComprobantesFiscalDto.Add(UtilComprobanteFiscal.MapearComprobanteFiscalDto(item));
+            }
+            return Ok(new Pagination<ComprobanteFiscalToReturnDto>(parameters.PageIndex, parameters.PageSize, count, lstComprobantesFiscalDto));
+        }
+    }
+}
---
+++ API/Controllers/Contribuyentes/ContribuyenteController.cs
@@ -0,0 +1,40 @@
+﻿using API.DTOs;
+using API.Helpers;
+using AutoMapper;
+using Core.Data;
+using Core.Entities.Contribuyentes;
+using Core.Entities.Interfaces;
+using Core.Specification.Contribuyentes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers.ContribuyentesController
+{
+    public class ContribuyenteController : BaseApiController
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly DbContextDGII dbContextDGII;
+        private readonly IMapper _mapper;
+
+        public ContribuyenteController(IUnitOfWork unitOfWork, DbContextDGII db, IMapper mapper)
+        {
+            this._unitOfWork = unitOfWork;
+            this.dbContextDGII = db;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("/contribuyentes")]
+        public async Task<ActionResult<Pagination<ContribuyenteToReturnDto>>> GetContribuyentes([FromQuery] ContribuyenteSpecificationParameters parameters)
+        {
+            var specification = new ContribuyenteWithTipoContribuyenteSpecification(parameters);
+            var contribuyentes = await _unitOfWork.Repository<Contribuyente>().ListAsync(specification);
+            var count = await _unitOfWork.Repository<Contribuyente>().CountAsync(specification);
+            var data=_mapper.Map<IReadOnlyList<Contribuyente>, IList<ContribuyenteToReturnDto>>(contribuyentes);
+            return Ok(new Pagination<ContribuyenteToReturnDto>(parameters.PageIndex, parameters.PageSize, count, data));
+
+
+        }
+    }
+}
