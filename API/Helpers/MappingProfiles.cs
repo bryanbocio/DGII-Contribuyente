0165a0f8e891f1242e67d582d6f76f6a231ca0b2
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 15:34:41 -0400

    creando perfiles de automapper

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,9 +1,20 @@
-﻿using AutoMapper;
+﻿using API.DTOs;
+using AutoMapper;
+using Core.Entities.Contribuyentes;
 
 namespace API.Helpers
 {
     public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+            CreateMap<Contribuyente, ContribuyenteToReturnDto>()
+                .ForMember(contriDto => contriDto.TipoContribuyente, option => option.MapFrom(contri => contri.TipoContribuyente.Descripcion)) ;
 
+            CreateMap<ComprobanteFiscal, ComprobanteFiscalToReturnDto>()
+                   .ForMember(comprobanteDto => comprobanteDto.RncCedula, option => option.MapFrom(compro => compro.Contribuyente.RncCedula)).ReverseMap();
+                    
+        
+        }
     }
 }
