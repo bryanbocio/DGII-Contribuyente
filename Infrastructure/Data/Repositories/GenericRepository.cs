41a37fac867ab71fa551b197014352a68edeaa36
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 15:36:46 -0400

    agregando generic reposiory

--- Infrastructure/Data/Repositories/GenericRepository.cs
+++ Infrastructure/Data/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Data;
 using Core.Entities;
 using Core.Entities.Interfaces;
+using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data.Repositories
@@ -14,6 +15,22 @@ namespace Infrastructure.Data.Repositories
         {
             _dbContext = dbContext;
         }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> specification)
+        {
+            return await ApplySpecification(specification).CountAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
         public async Task<T> GetByIdAsync(int id)
         {
             var entity = await _dbContext.Set<T>().FindAsync(id);
@@ -21,9 +38,30 @@ namespace Infrastructure.Data.Repositories
             return entity;
         }
 
+        public async Task<T> GetEntityWithSpecification(ISpecification<T> specification)
+        {
+            return await ApplySpecification(specification).FirstOrDefaultAsync();
+        }
+
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification)
+        {
+            return await ApplySpecification(specification).ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+        {
+            return SpecificationEvaluator<T>.getQuery(_dbContext.Set<T>().AsQueryable(), specification);
         }
     }
 }
