3d7f63db2dc7c6c88b1a9b084e544a782266504c
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 15:36:12 -0400

    cambiando de carpeta las entidades y agregando patron specification

---
+++ Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+        Task<int> Complete();
+    }
+}
---
+++ Core/Specification/Contribuyentes/ContribuyenteWithTipoContribuyenteSpecification.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Contribuyentes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification.Contribuyentes
+{
+    public class ContribuyenteWithTipoContribuyenteSpecification: BaseSpecification<Contribuyente>
+    {
+
+        public ContribuyenteWithTipoContribuyenteSpecification(ContribuyenteSpecificationParameters parameters): base(contribuyente =>
+
+        (string.IsNullOrEmpty(parameters.Search) || contribuyente.Nombre.ToLower().Contains(parameters.Search))
+            &&
+         (!parameters.TipoContribuyenteId.HasValue || contribuyente.TipoContribuyenteId == parameters.TipoContribuyenteId)
+
+        )
+        {
+            AddInclude(c=> c.TipoContribuyente);
+            AddOrderBy(product => product.Nombre);
+            ApplyPaging(parameters.PageSize * (parameters.PageIndex - 1), parameters.PageSize);
+        }
+
+        public ContribuyenteWithTipoContribuyenteSpecification(int id) : base(c => c.Id == id)
+        {
+            AddInclude(c => c.TipoContribuyente);
+        }
+    }
+}
