00f7cebd47d3360c53c5aa6d7749e356b7f32b27
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 15:35:09 -0400

    agregando clase utilitaria

---
+++ API/Utils/UtilComprobanteFiscal.cs
@@ -0,0 +1,18 @@
+﻿using API.DTOs;
+using Core.Entities.Contribuyentes;
+
+namespace API.Utils
+{
+    public  class UtilComprobanteFiscal
+    {
+
+        private UtilComprobanteFiscal()
+        {
+        }
+
+        public static ComprobanteFiscalToReturnDto MapearComprobanteFiscalDto(ComprobanteFiscal comprobanteFiscal)
+        {
+            return new ComprobanteFiscalToReturnDto { Monto = comprobanteFiscal.Monto, Ncf = comprobanteFiscal.Ncf, RncCedula = comprobanteFiscal.Contribuyente.RncCedula, Itbis = comprobanteFiscal.GetItbis() };
+        }
+    }
+}
