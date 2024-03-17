f199be41285d637ac3ac7f384d549ded6f4a9f7a
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 15:36:57 -0400

    agregando specification evaluator

---
+++ Infrastructure/Data/Specification/SpecificationEvaluator.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> getQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spectification)
+        {
+            var query = inputQuery;
+
+            if (spectification.criteria != null)
+            {
+                query = query.Where(spectification.criteria);
+            }
+
+            if(spectification.OrderBy != null)
+            {
+                query=query.OrderBy(spectification.OrderBy);
+            }
+
+            if (spectification.OrderByDescending != null)
+            {
+                query=query.OrderByDescending(spectification.OrderByDescending);
+            }
+            if (spectification.IsPagingEnabled)
+            {
+                query=query.Skip(spectification.Skip).Take(spectification.Take);
+            }
+
+            query = spectification.includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
