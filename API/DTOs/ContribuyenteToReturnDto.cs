d258cbbe4d2864efce441b34a974a6f01443e36c
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 23:09:48 -0400

    agregando id a dto

--- API/DTOs/ContribuyenteToReturnDto.cs
+++ API/DTOs/ContribuyenteToReturnDto.cs
@@ -2,6 +2,7 @@
 {
     public class ContribuyenteToReturnDto
     {
+        public int Id { get; set; }
         public string RncCedula { get; set; }
         public string Nombre { get; set; }
         public string TipoContribuyente { get; set; }
