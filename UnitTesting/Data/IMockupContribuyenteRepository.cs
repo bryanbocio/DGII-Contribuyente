ca9b20c22032e26234c7e7cca30f1985caaae108
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 23:10:41 -0400

    agregando unittesting

---
+++ UnitTesting/Data/IMockupContribuyenteRepository.cs
@@ -0,0 +1,16 @@
+﻿using API.DTOs;
+using Core.Entities.Contribuyentes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Data
+{
+    public interface IMockupContribuyenteRepository
+    {
+        Task<IEnumerable<ContribuyenteToReturnDto>> GetAllContribuyentes();
+        Task<IEnumerable<ContribuyenteToReturnDto>> GetContribuyenteById(string rncCedula);   
+    }
+}
