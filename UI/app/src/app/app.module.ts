3b8c7549f607d069a6d87ea28ea6c72b30c1654e
bryanbocio <alexanderbocio@gmail.com>
2024-03-17 23:34:28 -0400

    agregando spinner de carga

--- UI/app/src/app/app.module.ts
+++ UI/app/src/app/app.module.ts
@@ -1,28 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContribuyenteComponent } from './contribuyente/contribuyente.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { ComprobanteComponent } from './comprobante/comprobante.component';
+import {  NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/interceptor/loanding.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContribuyenteComponent,
     ComprobanteComponent,
-    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CoreModule
+    CoreModule,
+    NgxSpinnerModule
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports:[
+    NgxSpinnerModule
+  ],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi:true},
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
